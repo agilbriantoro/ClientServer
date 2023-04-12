1664169b1e59f2dd073e0a2b9f368a6646464510
agilbriantoro <briantoroagil@gmail.com>
2023-04-13 05:27:44 +0700

    Perubahan

--- Client/Repositories/Data/AccountRepository.cs
+++ Client/Repositories/Data/AccountRepository.cs
@@ -5,31 +5,30 @@ using Microsoft.AspNetCore.DataProtection.KeyManagement;
 using Newtonsoft.Json;
 using System.Text;
 
-namespace Client.Repositories.Data
-{
-    public class AccountRepository : GeneralRepository<Account, int>
-    {
-        private readonly HttpClient _httpClient;
-        private readonly string request;
-        public AccountRepository(string request = "Account/") : base(request)
-        {
-            this.request = request;
-            _httpClient = new HttpClient
-            {
-                BaseAddress = new Uri("https://localhost:7020/api/")
-            };
-        }
+namespace Client.Repositories.Data;
 
-        public async Task<ResponseVM<string>> Login(LoginVM entity)
+public class AccountRepository : GeneralRepository<Account, int>
+{
+    private readonly HttpClient _httpClient;
+    private readonly string request;
+    public AccountRepository(string request = "Account/") : base(request)
+    {
+        this.request = request;
+        _httpClient = new HttpClient
         {
-            ResponseVM<string> entityVM = null;
-            StringContent content = new StringContent(JsonConvert.SerializeObject(entity), Encoding.UTF8, "application/json");
-            using (var response = _httpClient.PostAsync(request + "Login/", content).Result)
-            {
-                string apiResponse = await response.Content.ReadAsStringAsync();
-                entityVM = JsonConvert.DeserializeObject<ResponseVM<string>>(apiResponse);
-            }
-            return entityVM;
+            BaseAddress = new Uri("https://localhost:7020/api/")
+        };
+    }
+
+    public async Task<ResponseVM<string>> Login(LoginVM entity)
+    {
+        ResponseVM<string> entityVM = null;
+        StringContent content = new StringContent(JsonConvert.SerializeObject(entity), Encoding.UTF8, "application/json");
+        using (var response = _httpClient.PostAsync(request + "Login/", content).Result)
+        {
+            string apiResponse = await response.Content.ReadAsStringAsync();
+            entityVM = JsonConvert.DeserializeObject<ResponseVM<string>>(apiResponse);
         }
+        return entityVM;
     }
 }
